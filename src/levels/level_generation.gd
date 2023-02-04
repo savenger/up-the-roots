19fe8aad13118497fd2c2e71b775a701e452b1f3
Sascha <github@smahmood.de>
2023-02-04 12:16:36 +0100

    fixed level generation

--- src/levels/level_generation.gd
+++ src/levels/level_generation.gd
@@ -3,22 +3,20 @@ class_name LevelGeneration extends Node
 const CHUNK_SIZE = 10
 const TILE_SIZE = 128
 var rng = RandomNumberGenerator.new()
-var cb = preload("res://src/levels/city_building.tscn")
+var tiles = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	tiles.append(preload("res://src/levels/city_building.tscn"))
 
 
 func generate_buildings(position: Vector2):
 	print("have go generate bulding in %s, %s and around" % [str(position.x), str(position.y)])
 	if not buldings_present_in_chunk(position):
 		generate_buildings_in_chunk(position)
-	var x = floor(position.x / (CHUNK_SIZE * TILE_SIZE))
-	var z = floor(position.y / (CHUNK_SIZE * TILE_SIZE))
 	for i in range(3):
 		for j in range(3):
-			var chunk_pos = position + Vector2(x + i - 1, z + j - 1)
+			var chunk_pos = Vector2(position.x + i - 1, position.y + j - 1)
 			print("around would be %s, %s" % [str(chunk_pos.x), str(chunk_pos.y)])
 			if not buldings_present_in_chunk(chunk_pos):
 				generate_buildings_in_chunk(chunk_pos)
@@ -33,6 +31,16 @@ func buldings_present_in_chunk(chunk_position):
 			return true
 	return false
 
+func get_random_tile():
+	var t = tiles[randi() % len(tiles)].instance()
+	var r = rng.randf_range(0, 4.0)
+	if r <= 1.0:
+		t.rotation = Vector3(0, PI / 2, 0)
+	elif r <= 2.0:
+		t.rotation = Vector3(0, PI, 0)
+	elif r <= 3.0:
+		t.rotation = Vector3(0, 3 * PI / 2, 0)
+	return t
 
 func generate_buildings_in_chunk(chunk_position):
 	#print("generate_buildings_in_chunk (x: %s, y: %s)" % [str(chunk_position.x), str(chunk_position.y)])
@@ -40,24 +48,12 @@ func generate_buildings_in_chunk(chunk_position):
 	for x in range(CHUNK_SIZE):
 		for z in range(CHUNK_SIZE):
 			var r = rng.randf_range(0, 4.0)
-			var c = cb.instance()
-			if r <= 1.0:
-				c = cb.instance() # TODO: use different building objects
-			elif r <= 2.0:
-				c = cb.instance() # TODO: use different building objects
-			elif r <= 3.0:
-				c = cb.instance() # TODO: use different building objects
-			r = rng.randf_range(0, 4.0)
-			if r <= 1.0:
-				c.rotation = Vector3(0, PI / 2, 0)
-			elif r <= 2.0:
-				c.rotation = Vector3(0, PI, 0)
-			elif r <= 3.0:
-				c.rotation = Vector3(0, 3 * PI / 2, 0)
-			add_child(c)
-			c.global_transform.origin.x = chunk_position.x * CHUNK_SIZE * TILE_SIZE + x * TILE_SIZE
-			c.global_transform.origin.z = chunk_position.y * CHUNK_SIZE * TILE_SIZE + z * TILE_SIZE
-			print("that is: %s, %s" % [str(c.global_transform.origin.x), str(c.global_transform.origin.z)])
+			var t = get_random_tile()
+			add_child(t)
+			t.global_transform.origin.x = chunk_position.x * CHUNK_SIZE * TILE_SIZE + x * TILE_SIZE
+			t.global_transform.origin.z = chunk_position.y * CHUNK_SIZE * TILE_SIZE + z * TILE_SIZE
+			if x == 0 and z == 0:
+				print("that is: %s, %s" % [str(t.global_transform.origin.x), str(t.global_transform.origin.z)])
 	if not LevelData.map.has(chunk_position.x):
 		LevelData.map[chunk_position.x] = Dictionary()
 	LevelData.map[chunk_position.x][chunk_position.y] = 1
