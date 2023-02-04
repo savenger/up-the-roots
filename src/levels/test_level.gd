8cb4a9a46aea0f2c739f2270c6f85b0ef4b22a6f
Sascha <github@smahmood.de>
2023-02-04 12:00:06 +0100

    fixed level generation

--- src/levels/test_level.gd
+++ src/levels/test_level.gd
@@ -1,6 +1,5 @@
 extends Spatial
 
-var rng = RandomNumberGenerator.new()
 var current_player_chunk_pos: Vector2
 var cb = preload("res://src/levels/city_building.tscn")
 
@@ -8,21 +7,19 @@ var cb = preload("res://src/levels/city_building.tscn")
 var last_output = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Player.connect("debug_output", self, "_on_debug_output_pressed")
 	current_player_chunk_pos.x = -1000
 	current_player_chunk_pos.y = -1000
-	rng.randomize()
-#	for x in range(50):
-#		for y in range(50):
-#			var c = cb.instance()
-#			if rng.randf_range(0, 10.0) >= 2.0:
-#				c.rotation = Vector3(0, PI / 2, 0)
-#			c.transform.origin.x = c.transform.origin.x + x * 128
-#			c.transform.origin.z = c.transform.origin.z + y * 128
-#			add_child(c)
+
+func _on_debug_output_pressed():
+	var chunk_pos: Vector2 = pos_to_chunk_pos($Player.global_transform.origin)
+	print($Player.global_transform.origin)
+	print(chunk_pos)
+	print(current_player_chunk_pos)
 
 func pos_to_chunk_pos(position):
-	var x = int(position.x / (10 * 128))
-	var z = int(position.z / (10 * 128))
+	var x = floor(position.x / (10 * 128))
+	var z = floor(position.z / (10 * 128))
 	return Vector2(x, z)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,10 +29,6 @@ func _process(delta):
 	last_output += delta
 	if last_output > 1:
 		last_output = 0
-		print($Player.global_transform.origin)
-		print(chunk_pos)
-		print(current_player_chunk_pos)
 	if chunk_pos.x != current_player_chunk_pos.x or chunk_pos.y != current_player_chunk_pos.y:
 		current_player_chunk_pos = chunk_pos
-		print("LOPFOR")
 		$level_generator.generate_buildings(chunk_pos)
