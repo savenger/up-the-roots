98a49ccdfb2e1b7b512eaa8edeb1933febf7f8f7
Sascha <github@smahmood.de>
2023-02-05 18:51:54 +0100

    improved randomizer

--- src/levels/level_generation.gd
+++ src/levels/level_generation.gd
@@ -54,6 +54,7 @@ var collectables = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rng.randomize()
 	collectables.append(preload("res://src/collectables/collectable.tscn"))
 
 
